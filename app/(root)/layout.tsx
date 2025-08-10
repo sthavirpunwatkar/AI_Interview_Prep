03c2a8d7984f637370cc34c375ce22bf68434dbb
sthavirpunwatkar <punwatkarsthavir@gmail.com>
2025-08-10 19:57:17 +0530

    Homepage, Interview cards , Logo of Techstack and Date

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import  {ReactNode} from 'react'
 
 const RootLayout = ({ children } : { children: ReactNode }) => {
     return ( 
-        <div>{children}</div>
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                    <Image src="/logo.svg" alt="logo" height={32} width={38}/>
+
+                    <h2 className="text-primary-100">InterPrep</h2>
+                </Link>
+            </nav>
+
+            {children}
+        </div>
      )
 }
  
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,53 @@
+import InterviewCard from '@/components/InterviewCard'
+import { Button } from '@/components/ui/button'
+import { dummyInterviews } from '@/constants'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
 
 const Page = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <section className="card-cta">
+        <div className="flex flex-col gap-6 max-w-lg">
+          <h2>Interview Ready with AI-Powered Practice & Feedback System
+
+          </h2>
+          <p className="text-lg">
+            Practice on Company Specific Questions and Curated Real Questions.
+          </p>
+
+          <Button asChild className="btn-primary max-sm:w-full">
+            <Link href="/interview">Start an Interview</Link>
+          </Button>
+        </div>
+
+        <Image src="/robot.png" alt='robo-dude' width={300} height={300} className="max-sm:hidden" />
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Your Inteviews</h2>
+
+        <div className='interviews-section'>
+          {dummyInterviews.map((interview) => (
+            <InterviewCard { ... interview} key={interview.id} />
+          ))}
+        </div>
+      </section>
+
+      <section className='flex flex-col gap-6 mt-8'>
+        <h2>Take an Interview</h2>
+
+        <div className='interviews-section'>
+         {dummyInterviews.map((interview) => (
+            <InterviewCard { ... interview} key={interview.id} />
+          ))}
+
+          {/* <p>You haven&apos;t taken any interviews yet.</p> */}
+        </div>
+      </section>
+    </>
   )
 }
 
